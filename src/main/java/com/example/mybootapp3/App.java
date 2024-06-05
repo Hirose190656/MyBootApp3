e701374bc265e8908cd6e145fe0529b8e3f12306
Hirose <190656@jc-21.jp>
2024-06-05 12:32:04 +0900

    kdai5

--- src/main/java/com/example/mybootapp3/App.java
+++ src/main/java/com/example/mybootapp3/App.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
  
-/**
+/*
 * Hello world!
 *
 */
